7cdf8f49f9c1b9504f4b452dbd03476b789b95d2
tonyRaafat <tonyrafat01@gmail.com>
2024-08-13 23:37:57 +0300

    fix: auth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,14 +3,14 @@ import { throwError } from '../utils/throwerror.js';
 import { User } from '../../database/models/user.model.js';
 
 export const auth = async (req, res, next) => {
+  try {
   if (req?.header('token') == undefined) { throw throwError('token was not provided in header', 400) }
   const token = req.header('token');
   if (!token) return res.statusCode(401).send('Access denied. No token provided.');
-  try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET, { expiresIn: '2h' });
     if (!decoded?.email) throw throwError('invalid token payload', 400)
     const user = await User.findById(decoded._id)
-    if(parseInt(user.passwordChanged.getTime()/1000)> decoded.iat) throw throwError("token is invalid",403)
+    if(parseInt(user.passwordChanged?.getTime()/1000)> decoded.iat) throw throwError("token is invalid",403)
     req.user = user
     next();
   } catch (error) {
