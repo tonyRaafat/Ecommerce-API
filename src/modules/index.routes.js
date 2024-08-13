432dedbf4ecd83c76e9121dcee8d0a8b4ff91a81
tonyRaafat <tonyrafat01@gmail.com>
2024-08-14 00:37:23 +0300

    feat: add all routes

--- src/modules/index.routes.js
+++ src/modules/index.routes.js
@@ -1,5 +1,12 @@
 import usersRoutes from './users/users.routers.js';
 import categoryRoutes from './category/categories.routers.js';
-import brandRoutes from './brands/brand.routes.js'
+import brandRoutes from './brands/brand.routers.js'
 import subCategoryRoutes from './subCategory/subCategories.routers.js';
-export {usersRoutes, categoryRoutes, subCategoryRoutes, brandRoutes}
+import productRoutes from './product/products.routers.js';
+import cartRoutes from './cart/cart.routers.js'
+import orderRoutes from './order/order.routers.js';
+import reviewRoutes from './review/reviews.routers.js'
+import couponRoutes from './coupon/coupon.routers.js'
+import wishListRoutes from './wishList/wishList.routers.js'
+
+export { usersRoutes, categoryRoutes, subCategoryRoutes, brandRoutes, productRoutes, cartRoutes, orderRoutes, reviewRoutes, couponRoutes, wishListRoutes }
