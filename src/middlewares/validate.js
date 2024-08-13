c92fff59113c105ebb816c51df4c18970c9a2461
tonyRaafat <tonyrafat01@gmail.com>
2024-08-13 23:39:05 +0300

    feat: enhance validate

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,13 +1,21 @@
-import { throwError } from "../utils/throwerror.js"
+const dataMethod = ['body', 'query', 'headers', 'params', 'file', 'files']
 
-
-export const validate = (schema)=>{
-    return (req,res,next)=>{
-        let {error} = schema.validate({...req.body,...req.params,...req.query})
-        if(error){
-            next(new throwError(error,400))
-        }else{
-            next()
+export const validate = (schema) => {
+  return (req,res,next) => {
+    let arrayErr = []
+    dataMethod.forEach((key) => {
+      if(schema[key]){
+        const {error} = schema[key].validate(req[key], {aboortEarly: false})
+        if(error?.details){
+          error.details.forEach((err) => {
+            arrayErr.push(err.message)
+          })
         }
+      }
+    })
+    if(arrayErr.length != 0){
+      return res.status(400).json({msg:"invalid input data", err: arrayErr})
     }
+    next()
+  }
 }
