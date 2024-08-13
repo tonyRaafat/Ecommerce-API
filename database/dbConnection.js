5b07b9babf3a08fec5623d1f29237e4e4ab14ad0
tonyRaafat <tonyrafat01@gmail.com>
2024-08-14 00:42:40 +0300

    feat: change monogodb uri from localhost to online

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-export const dbConnection  =async ()=> await mongoose.connect(process.env.MONGODB_URI)
+export const dbConnection  =async ()=> await mongoose.connect(process.env.MONGODB_URI_ONLINE)
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
