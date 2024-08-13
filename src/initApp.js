7e8746342b68d4903bb4be85db78358066e57c97
tonyRaafat <tonyrafat01@gmail.com>
2024-08-13 23:41:08 +0300

    feat: add all routes

--- src/initApp.js
+++ src/initApp.js
@@ -2,27 +2,40 @@ import dotenv from 'dotenv';
 import { errorHandler } from './utils/errorhandler.js';
 import { dbConnection } from '../database/dbConnection.js';
 import * as routes from './modules/index.routes.js'
+import cors from 'cors'
+export const initApp = (app, express) => {
 
-export const initApp = (app,express)=>{ 
+  dotenv.config();
+  dbConnection();
 
-dotenv.config();
-dbConnection();
+  app.use(cors())
+  app.use(express.json());
 
-app.use(express.json());
+  app.get('/',(req,res,next)=>{
+    res.status(200).json({msg:"Server is running..."})
+  })
 
-app.use('/users', routes.usersRoutes);
-app.use('/categories', routes.categoryRoutes);
-app.use('/subCategories',routes.subCategoryRoutes)
-app.use('brands',routes.brandRoutes)
+  app.use('/users', routes.usersRoutes);
+  app.use('/categories', routes.categoryRoutes);
+  app.use('/subCategories', routes.subCategoryRoutes)
+  app.use('/brands', routes.brandRoutes)
+  app.use('/products', routes.productRoutes)
+  app.use('/orders', routes.orderRoutes)
+  app.use('/coupons', routes.couponRoutes)
+  app.use('/carts', routes.cartRoutes)
+  app.use('/orders', routes.orderRoutes)
+  app.use('/wishLists', routes.wishListRoutes)
+  app.use('/reviews', routes.reviewRoutes)
 
-app.all('*', (req, res, next) => {
-  const error = new Error(`Cannot ${req.method} ${req.originalUrl}`)
-  error.statusCode = 404
-  next(error)
-})
 
-app.use(errorHandler)
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+  app.all('*', (req, res, next) => {
+    const error = new Error(`Cannot ${req.method} ${req.originalUrl}`)
+    error.statusCode = 404
+    next(error)
+  })
+
+  app.use(errorHandler)
+
 }
