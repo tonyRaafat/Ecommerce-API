4ff635927c8960c55d26b38bab6aeb76de51e713
tonyRaafat <tonyrafat01@gmail.com>
2024-08-14 00:36:00 +0300

    fix: add app.listen

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ import express from 'express';
 import { initApp } from './src/initApp.js';
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 initApp(app,express)
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
